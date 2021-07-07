29f6f28225e3e6c0fa5e6aa1630b38c0137b2b06
saravana <saravanasundar133@gmail.com>
2021-07-07 14:57:15 +0530

    Angular js environment setup and module creation

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+var myApp = angular.module('myModule', []);
+
+// var myController = function ($scope) {
+//     $scope.message = 'Angular JS Tutorial';
+// }
+
+myApp.controller('myController', function ($scope) {
+    $scope.message = 'Angular JS Tutorial';
+});
