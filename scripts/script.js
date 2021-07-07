d9656033957929dd0cc7082f54e272526fd23169
saravana <saravanasundar133@gmail.com>
2021-07-07 15:06:30 +0530

    Angular js controller with method chaining

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,31 @@
-var myApp = angular.module('myModule', []);
+//method chaining to create module and controller
+
+var myApp = angular
+            .module('myModule', [])
+            .controller('myController', function ($scope) {
+                var employee = {
+                    'firstName': "David",
+                    "lastName": "Das",
+                    "gender": "Male"
+                }
+                $scope.message = 'Angular JS Tutorial';
+                $scope.employee = employee;
+            });
+
+//creating variable for container
 
 // var myController = function ($scope) {
 //     $scope.message = 'Angular JS Tutorial';
 // }
 
-myApp.controller('myController', function ($scope) {
-    $scope.message = 'Angular JS Tutorial';
-});
+//mapping module and controller without creating variable for controller
+
+// myApp.controller('myController', function ($scope) {
+//     var employee = {
+//         'firstName': "David",
+//         "lastName": "Das",
+//         "gender": "Male"
+//     }
+//     $scope.message = 'Angular JS Tutorial';
+//     $scope.employee = employee;
+// });
