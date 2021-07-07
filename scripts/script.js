1f5757dc6030e8b73a801d967aab9a21468aba21
saravana <saravanasundar133@gmail.com>
2021-07-07 15:42:54 +0530

    Angular js with ng repeat

--- scripts/script.js
+++ scripts/script.js
@@ -3,5 +3,15 @@
 var myApp = angular
             .module('myModule', [])
             .controller('myController', function ($scope) {
-                $scope.message = "Angular js tutorial";
+                var countries = [
+                    {
+                        name: "India",
+                        cities: ['Chennai', "Hydredabad", "Bangalore"]
+                    },
+                    {
+                        name: "USA",
+                        cities: ['New york', "Texas"]
+                    }
+                ]
+                $scope.countries = countries;
             });
