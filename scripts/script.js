b5cead2cf9cd5dac585326f98f373cb75c763a71
saravana <saravanasundar133@gmail.com>
2021-07-07 15:21:29 +0530

    Angular js two way binding

--- scripts/script.js
+++ scripts/script.js
@@ -3,27 +3,5 @@
 var myApp = angular
             .module('myModule', [])
             .controller('myController', function ($scope) {
-                var country = {
-                    'name': "India",
-                    "flag": "images/indian flag.png",
-                }
-                $scope.country = country;
+                $scope.message = "Angular js tutorial";
             });
-
-//creating variable for container
-
-// var myController = function ($scope) {
-//     $scope.message = 'Angular JS Tutorial';
-// }
-
-//mapping module and controller without creating variable for controller
-
-// myApp.controller('myController', function ($scope) {
-//     var employee = {
-//         'firstName': "David",
-//         "lastName": "Das",
-//         "gender": "Male"
-//     }
-//     $scope.message = 'Angular JS Tutorial';
-//     $scope.employee = employee;
-// });
